a9f3fe90ec497292ccfff01e35e9d678dba5daf0
Travis Hinton <t@travishinton.us>
2021-10-14 15:06:31 -0600

    Half through operation functionality

--- main.js
+++ main.js
@@ -8,9 +8,18 @@ const resultsContainer = document.querySelector('.resultsContainer');
 const results = document.querySelector('.results');
 
 // Setting global variables
-let currentDisplay = '0'
-let currentTotal = 0
+let currentDisplay = 0;
+let currentTotal = 0;
+let numberOne
+let numberTwo
+let numberThree
+let numberFour
+let numberFive
+let numberSix
+let numberSeven
+let operator
 results.textContent = currentDisplay
+
 
 // Evaluation functions
 const add = (num, num2) => {
@@ -28,14 +37,18 @@ const divide = (num, num2) => {
 const operate = (func, num, num2) => {
     return func(num, num2)
 }
-
-// Buttons appear in results display
-let updateDisplay = (id) => {
-    currentDisplay = currentDisplay + id;
+const clearAll = () => {
+    currentDisplay = 0;
+    currentTotal = 0;
+    numberOne = 0;
+    numberTwo = 0;
 }
 
+// Display results functions
+let updateResults = () => { results.textContent = currentDisplay }
+
 // Create HTMl calculator
-// Create number buttons
+// Create operator buttons
 const operatorsArray = ['+', '-', '*', '/', '=', 'C'];
 for (let i = 0; i < 6; i++) {
     newDiv = document.createElement('div');
@@ -46,7 +59,7 @@ for (let i = 0; i < 6; i++) {
     operators.appendChild(newDiv);
 }
 
-// Create operator buttons
+// Create number buttons
 for (let index = 9; index >= 0; index--) {
     newDiv = document.createElement('div');
     newDiv.id = `${index}`
@@ -57,14 +70,45 @@ for (let index = 9; index >= 0; index--) {
     numberInput.appendChild(newDiv);
 }
 
-document.querySelectorAll('.button').forEach(item => {
+// Add eventListeners to number buttons to update display
+document.querySelectorAll('.numberButton').forEach(item => {
     item.addEventListener('click', event => {
-        if (currentDisplay == 0) {
-            currentDisplay = item.id
+
+        if (!numberOne) {
+            if (currentDisplay == 0) {
+                currentDisplay = item.id
+                updateResults();
+            } else {
+                currentDisplay += item.id;
+                updateResults();
+            }
+
+        } else if (numberOne) {
+            if (numberOne && currentDisplay == 0) {
+                currentDisplay = item.id
+                updateResults();
+            } else {
+                currentDisplay += item.id
+                updateResults();
+            }
+        }
+    })
+})
+
+// Add listeners to operator buttons
+document.querySelectorAll('.operatorButton').forEach(item => {
+    item.addEventListener('click', event => {
+        if (item.id == "C") {
+            clearAll();
             results.textContent = currentDisplay;
         } else {
-            currentDisplay += item.id;
-            results.textContent = currentDisplay;
+            if (!numberOne) {
+                numberOne = Number(currentDisplay)
+                currentDisplay = 0;
+            } else {
+                numberOne = numberOne;
+            }
+
         }
     })
 })
